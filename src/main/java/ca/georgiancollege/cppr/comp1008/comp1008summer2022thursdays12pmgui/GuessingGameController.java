0508a4596d7a839f6a793d9d6a94da7216e0856b
Ben Blanc <ben.blanc@georgebrown.ca>
2022-08-11 14:29:37 -0400

    Class Code August 11, 2022 @ 14:30

--- src/main/java/ca/georgiancollege/cppr/comp1008/comp1008summer2022thursdays12pmgui/GuessingGameController.java
+++ src/main/java/ca/georgiancollege/cppr/comp1008/comp1008summer2022thursdays12pmgui/GuessingGameController.java
@@ -2,14 +2,20 @@ package ca.georgiancollege.cppr.comp1008.comp1008summer2022thursdays12pmgui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 import java.nio.file.Paths;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 
 public class GuessingGameController {
@@ -57,24 +63,80 @@ public class GuessingGameController {
 
     private int userClickCount = 0;
 
+    private String[] colors = {"red", "grey", "green", "blue", "orange", "pink", "brown", "purple" };
 
-    String imageDirectoryPath = String.valueOf(Paths.get("src/main/resources/ca/georgiancollege/cppr/comp1008/comp1008summer2022thursdays12pmgui/images/guessinggame/"));
+    private String imageExtension = ".png";
+    String imageDirectoryPath = Paths.get("src/main/resources/ca/georgiancollege/cppr/comp1008/comp1008summer2022thursdays12pmgui/images/guessinggame/").toAbsolutePath() + "\\";
+    String transparentBlackStyle = "-fx-background-color: rgba(0,0,0,0.5)";
 
+    void showCard(MouseEvent e){
+        ImageView clicked = ((ImageView)e.getSource());
+        String clickedID = clicked.getId();
+        int cardIndex = Integer.parseInt(clickedID.substring(5));
+        //System.out.println(cardIndex);
+
+        String whatImageToDisplay = boxSlots[cardIndex];
+        //System.out.println(imageDirectoryPath + whatImageToDisplay);
+        clicked.setImage(
+                new Image(imageDirectoryPath + whatImageToDisplay)
+        );
+
+        boxColorList.add(whatImageToDisplay);
+        imageViewList.add(clicked);
+
+    }
 
     @FXML
-    void trackUserClicks(){
+    void trackUserClicks(MouseEvent e){
+
+        message.setText("");
+        showCard(e);
+
         userClickCount++;
+        //System.out.println("clicked on ID of " + ((ImageView)e.getSource()).getId());
 
         if(userClickCount == 2){
             check.setDisable(false);
             grid.setDisable(true);
-            grid.setStyle("-fx-background-color: rgba(0,0,0,0.5)");
+            grid.setStyle(transparentBlackStyle);
             userClickCount = 0;
             incrementStat(attempts);
+
         }
     }
     @FXML
     void checkSolution(ActionEvent event) {
+        if(boxColorList.get(0).equals(boxColorList.get(1))){
+            incrementStat(correct);
+
+            for(ImageView current : imageViewList){
+                current.setImage(new Image(imageDirectoryPath + "correct.png"));
+                current.setDisable(true);
+                current.setStyle(transparentBlackStyle);
+
+            }
+            message.setTextFill(Color.rgb(0, 255, 0));
+
+        }
+        else{
+
+            for(ImageView current : imageViewList){
+                current.setImage(new Image(imageDirectoryPath + "blank.png"));
+            }
+
+
+            message.setText("Incorrect Guess! Please try again");
+            message.setTextFill(Color.rgb(255, 0, 0));
+
+        }
+
+
+        boxColorList.clear();
+        imageViewList.clear();
+
+        grid.setDisable(false);
+        grid.setStyle("");
+        check.setDisable(true);
 
     }
 
@@ -107,6 +169,39 @@ public class GuessingGameController {
             boxSlots[i] = "";
         }
 
+
+        SecureRandom sran = new SecureRandom();
+        for (String color : colors){
+            int colorCount = 0;
+
+            while(colorCount < 2){
+                int randomIndex = sran.nextInt(numberOfBoxes);
+
+                if(boxSlots[randomIndex].length() == 0){
+                    boxSlots[randomIndex] = color + imageExtension;
+                    colorCount++;
+                }
+            }
+            
+            
+        }
+
+        int counter = 0;
+        for(Node element : grid.getChildren()){
+
+            try {
+                ImageView current = (ImageView) element;
+                current.setId("card_" + counter);
+                counter++;
+                current.setOnMouseClicked(this::trackUserClicks);
+
+            }
+            catch (Exception e){
+                System.err.println("Error in explicitly typecasting Node to Image View in populateColors");
+            }
+            
+        }
+        
     }
     @FXML
     void initialize(){
